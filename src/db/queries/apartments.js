357b0a9fab78a89c31ab889c418fb727791e4257
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2021-02-16 17:24:29 +0100

    feature: add a new apartment

--- src/db/queries/apartments.js
+++ src/db/queries/apartments.js
@@ -1,6 +1,6 @@
 /**
  * @method getAllApartments
- * @description Method get all apartments
+ * @description Method to get all apartments
  * @param {object} dbInstance - The database instance
  * @param {object} request - The request object
  * @param {object} response - The response object
@@ -42,6 +42,50 @@ const getAllApartments = (dbInstance) => (request, response) => {
   }
 };
 
+/**
+ * @method addNewApartment
+ * @description Method to add a new apartment
+ * @param {object} dbInstance - The database instance
+ * @param {object} request - The request object
+ * @param {object} response - The response object
+ * @returns {object} - Response object
+ */
+const addNewApartment = (dbInstance) => (request, response) => {
+  const {
+    type, address, state,
+  } = request.body;
+
+  try {
+    dbInstance.query('INSERT INTO apartments (owner_id, type, address, state) VALUES ($1, $2, $3, $4) RETURNING * ', [request.userId, type, address, state], (error, results) => {
+      if (error) {
+        const { message, stack, code } = error;
+
+        return response.status(400).send({
+          status: 'error',
+          message,
+          code,
+          data: stack,
+        });
+      }
+
+      return response.send({
+        message: 'success',
+        data: results.rows[0],
+      });
+    });
+  } catch (error) {
+    const { message, stack, code } = error;
+
+    return response.status(400).send({
+      status: 'error',
+      message,
+      code,
+      data: stack,
+    });
+  }
+};
+
 module.exports = {
   getAllApartments,
+  addNewApartment,
 };
--- src/routes/apartment.js
+++ src/routes/apartment.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const pool = require('../config/db');
-const { getAllApartments } = require('../db/queries/apartments');
+const { getAllApartments, addNewApartment } = require('../db/queries/apartments');
 const { verifyToken } = require('../middlewares/verifyToken');
 
 const router = new express.Router();
+
+router.post('/apartments/new', verifyToken, addNewApartment(pool));
 
 router.get('/apartments', verifyToken, getAllApartments(pool));
 
