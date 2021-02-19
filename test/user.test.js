fb852f6319f9bedc2db10d519fad4a98c87e048d
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2021-02-19 22:39:43 +0100

    unit tests passed for user routes

--- test/user.test.js
+++ test/user.test.js
@@ -1,5 +1,18 @@
 const request = require('supertest');
+const bcrypt = require('bcryptjs');
 const app = require('../src/app');
+const db = require('../src/config/db');
+
+afterEach(async () => {
+  await db.query('TRUNCATE TABLE users');
+
+  const encodedPassword = bcrypt.hashSync('password', 8);
+
+  await db.query(
+    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+    ['Temi', 'temi@app.net', encodedPassword],
+  );
+});
 
 describe('User Routes', () => {
   it('should register new user', async () => {
@@ -11,5 +24,47 @@ describe('User Routes', () => {
         password: 'MyPass777!',
       })
       .expect(201);
+  });
+
+  it('should not register user', async () => {
+    await request(app)
+      .post('/register')
+      .send({
+        name: 'Tosin',
+        email: 'temi@app.net',
+        password: 'MyPass777!',
+      })
+      .expect(403);
+  });
+
+  it('should give a validation error', async () => {
+    await request(app)
+      .post('/register')
+      .send({
+        name: 'Jale',
+        email: 'jale@example.zen',
+        password: 'MyPass777!',
+      })
+      .expect(422);
+  });
+
+  it('should login existing user', async () => {
+    await request(app)
+      .post('/login')
+      .send({
+        email: 'temi@app.net',
+        password: 'password',
+      })
+      .expect(200);
+  });
+
+  it('should not login non-existent user', async () => {
+    await request(app)
+      .post('/login')
+      .send({
+        email: 'kebbi@app.net',
+        password: 'thisisnotmypass',
+      })
+      .expect(401);
   });
 });
