f8c7a91d1a64199aa88b7da82a6fcdac152dae5f
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2021-02-22 16:03:36 +0100

    write more tests for apartments

--- test/apartment.test.js
+++ test/apartment.test.js
@@ -3,19 +3,48 @@ const bcrypt = require('bcryptjs');
 const app = require('../src/app');
 const db = require('../src/config/db');
 
+let userToken;
+let userId;
+
+beforeEach(async () => {
+  const userObj = await request(app).post('/register').send({
+    name: 'Tosin',
+    email: 'temi@app.net',
+    password: 'MyPass777!',
+  });
+
+  userId = userObj.body.data.user.id;
+  userToken = userObj.body.data.token;
+
+  await db.query(
+    'INSERT INTO apartments (type, address, state, owner_id) VALUES ($1, $2, $3, $4) RETURNING *',
+    ['2 bedroom', 'ajanlekoko abejoye', 'Lagos', userId],
+  );
+});
+
+afterEach(async () => {
+  await db.query('TRUNCATE TABLE apartments');
+
+  await db.query('TRUNCATE TABLE users');
+});
+
 describe('Apartment Routes', () => {
   it('should add a new apartment', async () => {
     await request(app)
       .post('/apartments/new')
-      .set(
-        'Authorization',
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUxLCJpYXQiOjE2MTQwMDI3NTIsImV4cCI6MTYxNDA4OTE1Mn0.kqQD5wp4b8JmMA0cx55nsM2pjDhK4DofdPBM-Aod_v0',
-      )
+      .set('Authorization', `Bearer ${userToken}`)
       .send({
         type: '2 bedroom',
         address: 'ajanlekoko abejoye',
         state: 'Lagos',
       })
       .expect(201);
+  });
+
+  it('should get all existing apartments', async () => {
+    await request(app)
+      .get('/apartments')
+      .set('Authorization', `Bearer ${userToken}`)
+      .expect(200);
   });
 });
